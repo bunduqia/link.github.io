1cbcb37b1a347c7facee4f0394c191c2028e56c5
bunduqia <bunduqias@gmail.com>
2025-04-03 09:00:43 +0300

    Update content.js

--- content.js
+++ content.js
@@ -1,1 +1,6 @@
-
+window.addEventListener("message", (event) => {
+    if (event.source !== window) return;
+    if (event.data.type && event.data.type === "FROM_PAGE") {
+        chrome.runtime.sendMessage(event.data);
+    }
+}, false);
