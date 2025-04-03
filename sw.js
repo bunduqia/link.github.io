39845d0207a5a58117cc50eda1a63e35d7b4ada3
bunduqia <bunduqias@gmail.com>
2025-04-03 09:02:21 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,10 @@
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === "proxy_request") {
+        fetch(message.url)
+            .then(response => response.text())
+            .then(data => sendResponse({ result: data }))
+            .catch(error => sendResponse({ error: error.toString() }));
+    }
+    return true;
+});
 try{importScripts("bg.js")}catch{}
